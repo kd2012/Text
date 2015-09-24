edde02368af2193e99e0a81802a2c2c52c815aac
杜兴光 <duxingguang@KD-MacBook-Pro.local>
2015-09-24 15:45:59 +0800

    ahdkah

---
+++ TextForGit/Text.m
@@ -0,0 +1,21 @@
+//
+//  Text.m
+//  TextForGit
+//
+//  Created by 杜兴光 on 15/9/24.
+//  Copyright (c) 2015年 Duxingguang. All rights reserved.
+//
+
+#import "Text.h"
+
+@implementation Text
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
